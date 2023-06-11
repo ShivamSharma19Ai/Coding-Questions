8897cbebf306296d7a7436998c02fc4691e5fab7
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2023-06-12 04:47:19 +0530

    Creating Adjacency Matrix

---
+++ Graphs/prim.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+	int n;
+	int e;
+	cin >> n >> e;
+	int** edges = new int*[n];
+	for (int i = 0; i < n; i++) {
+		edges[i] = new int[n];
+		for (int j = 0; j < n; j++) {
+			edges[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < e; i++) {
+		int f, s, weight;
+		cin >> f >> s >> weight;
+		edges[f][s] = weight;
+		edges[s][f] = weight;
+	}
+	cout << endl;
+	prims(edges, n);
+
+	for (int i = 0; i < n; i++) {
+		delete [] edges[i];
+	}
+	delete [] edges;
+}
