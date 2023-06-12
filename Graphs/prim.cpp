f7a9db4f94decb37e4b5686976258980fe5da8f4
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2023-06-12 05:30:48 +0530

    Completed prim Algorithm

--- Graphs/prim.cpp
+++ Graphs/prim.cpp
@@ -1,6 +1,56 @@
+
 #include<iostream>
 #include<climits>
 using namespace std;
+
+int findMinVertex(int* weights, bool* visited, int n){
+
+	int minVertex = -1;
+	for(int i = 0; i < n; i++){
+		if(!visited[i] && (minVertex == - 1 || weights[i] < weights[minVertex])){
+			minVertex = i;
+		}
+	}
+	return minVertex;
+}
+
+
+void prims(int** edges, int n){
+
+	int* parent = new int[n];
+	int* weights = new int[n];
+	bool* visited = new bool[n];
+
+	for(int i = 0; i < n; i++){
+		visited[i] = false;
+		weights[i] = INT_MAX;
+	}
+	parent[0] = -1;
+	weights[0] = 0;
+
+	for(int i = 0; i < n - 1; i++){
+		// Find Min Vertex
+		int minVertex = findMinVertex(weights, visited, n);
+		visited[minVertex] = true;
+		// Explore un visted neighbours
+		for(int j = 0; j < n; j++){
+			if(edges[minVertex][j] != 0 && !visited[j]){
+				if(edges[minVertex][j] < weights[j]){
+					weights[j] = edges[minVertex][j];
+					parent[j] = minVertex;
+				}
+			}
+		}
+	}
+
+	for(int i = 1; i < n; i++){
+		if(parent[i] < i){
+			cout << parent[i] << " "  << i << " " << weights[i] << endl;
+		}else{
+			cout << i << " " << parent[i] << " " << weights[i] << endl;
+		}
+	}
+}
 
 int main() {
 	int n;
