6eb1e83f2656d1da11146558dd5f01835d035b9f
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2023-09-10 22:05:36 +0530

    New Codes

--- tcs/armstrongnumber.cpp
+++ tcs/armstrongnumber.cpp
@@ -1,38 +1,72 @@
-#include<iostream>
+
+// C++ program to check whether the
+// number is an Armstrong number or not
+#include <iostream>
 using namespace std;
 
-int power(int x, unsigned int y){
-    if(y==0){ return 1;}
-    if(y%2 == 0){ 
-       return power(x,y/2)*power(x,y/2);
-    }
-    return x*power(x,y/2)*power(x,y/2);
+// Driver Code
+int main()
+{
+	int n = 153;
+	int temp = n;
+	int p = 0;
 
-}
+	// Function to calculate the sum of
+	// individual digits
+	while (n > 0) {
+		int rem = n % 10;
+		p = (p) + (rem * rem * rem);
+		n = n / 10;
+	}
 
-int order(int x){
-    int n=0;
-    while(x){
-        n++;
-        x=x/10;
-    }
-    return n;
+	// Condition to check whether the value
+	// of P equals to user input or not.
+	if (temp == p) {
+		cout << ("Yes. It is Armstrong No.");
+	}
+	else {
+		cout << ("No. It is not an Armstrong No.");
+	}
+	return 0;
 }
+// This code is contributed by sathiyamoorthics19
 
-bool isArmStrong(int x){
-    int n=order(x);
-    int temp=x;
-    int sum=0;
-    while(temp){
-        int r=temp%10;
-        sum += power(r,n);
-        temp=temp/10;
-    }
-    return (sum==x);
-}
 
-int main(){
-    int n;
-    cin>>n;
-    cout<<isArmStrong(n);
-}
+// #include<iostream>
+// using namespace std;
+
+// int power(int x, unsigned int y){
+//     if(y==0){ return 1;}
+//     if(y%2 == 0){ 
+//        return power(x,y/2)*power(x,y/2);
+//     }
+//     return x*power(x,y/2)*power(x,y/2);
+
+// }
+
+// int order(int x){
+//     int n=0;
+//     while(x){
+//         n++;
+//         x=x/10;
+//     }
+//     return n;
+// }
+
+// bool isArmStrong(int x){
+//     int n=order(x);
+//     int temp=x;
+//     int sum=0;
+//     while(temp){
+//         int r=temp%10;
+//         sum += power(r,n);
+//         temp=temp/10;
+//     }
+//     return (sum==x);
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<isArmStrong(n);
+// }
